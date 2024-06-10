e21d4560c5c1179cc99bb32a1180848cd630c74e
Artur Gaetov <agaetov@gmail.com>
2024-06-10 18:00:27 +0300

    created serializers and views for Lesson and Course

---
+++ materials/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from materials.models import Course, Lesson
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('title', 'description',)
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ('course', 'title', 'description',)
