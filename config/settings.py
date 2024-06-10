208f0e6015e45136fc1cd63f8644b4291d593820
Artur Gaetov <agaetov@gmail.com>
2024-06-10 17:01:27 +0300

    created model Course and Lesson

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "users", "materials",
+    "users",
+    "materials",
 ]
 
 MIDDLEWARE = [
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название курса")
+    preview = models.ImageField(
+        upload_to="materials_media/courses/previews",
+        verbose_name="Превью",
+        **NULLABLE,
+        help_text="Загрузите превью курса"
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        **NULLABLE,
+        help_text="Опишите основные материалы курса"
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+
+    title = models.CharField(max_length=100, verbose_name="Название урока")
+    description = models.TextField(
+        verbose_name="Описание урока",
+        **NULLABLE,
+        help_text="Опишите основные материалы урока"
+    )
+    preview = models.ImageField(
+        upload_to="materials_media/lessons/previews",
+        verbose_name="Превью урока",
+        **NULLABLE,
+        help_text="Загрузите превью урока"
+    )
+    video_link = models.URLField(
+        verbose_name="Ссылка на видео",
+        **NULLABLE,
+        help_text="Укажите ссылку на видео урока"
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.title
