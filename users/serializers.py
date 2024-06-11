2ba5cbdbc37248207bef31bb5b54f9f0d50f2679
Artur Gaetov <agaetov@gmail.com>
2024-06-11 23:04:28 +0300

    lesson#24.2 ex#5

--- users/serializers.py
+++ users/serializers.py
@@ -3,17 +3,19 @@ from rest_framework import serializers, filters
 from users.models import User, Payments
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     filter_backends = [filters.OrderingFilter]
-    filterset_fields = ('payment_method', 'paid_course', 'paid_lesson', )
+    filterset_fields = ('payment_method', 'paid_course', 'paid_lesson',)
     ordering_fields = ('data',)
 
     class Meta:
         model = Payments
         fields = ("id", 'data', 'payment_count', 'payment_method', 'paid_course', 'paid_lesson')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    payments_history = PaymentSerializer(many=True, source='payment')
+
+    class Meta:
+        model = User
+        fields = "__all__"
